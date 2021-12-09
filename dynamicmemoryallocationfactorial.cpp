31a5e2e72aa9e632f359ead31e8fe74748f77b0b
Mukta <mukta1897@gmail.com>
2021-12-09 15:11:29 +0530

    Create dynamicmemoryallocationfactorial.cpp

---
+++ dynamicmemoryallocationfactorial.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int result[1000] = {0};
+int fact(int n) {
+    if (n>=0){
+       result[0] = 1;
+      for (int i = 1; i <= n; ++i) {
+         result[i] = i * result[i - 1];
+      }
+       return result[n];
+    }
+}
+int main() {
+   int n;
+      cout<<"\nEnter integer to compute factorial : ";
+      cin>>n;
+      cout<<"Factorial of "<<n<<" is "<<fact(n)<<endl;
+     
+   return 0;
+}
